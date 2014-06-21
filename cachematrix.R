e9f281bc9cc42932c2caced72636b5cad8c1e14e
Shahar Frechter <sfrechter@gmail.com>
2014-06-21 01:34:47 +0100

    fixed minor typos

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,6 @@
-##  This function takes a matrix stores it (under variable "x") and returnes a list of functions. These functions are called by the "cacheSolve" function to handle the matrix inversion and to set the status of the matrix inversion (see "cacheSolve function" for explenation about the status mechanism).
+##  This function takes a matrix stores it (under variable "x") and returnes a list of functions. 
+##  These functions are called by the "cacheSolve" function to handle the matrix inversion and to set the status of the matrix inversion 
+##  (see "cacheSolve function" for explenation about the status mechanism).
 
 ## Stores matrix and returnes list of functions
 makeCacheMatrix <- function(x = matrix()) {
@@ -18,7 +20,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-##  This function calls the different "makeCacheMatrix" functions according to the status of m. When a new matrix is used m=null. Therefore the inverse matrix will be calculated and m will be set to the resut of this calculation (inverse matrix) and returned. When the same matrix is called again m=invers matrix and therefore not null. Therefore the function will simply return m (inverse matrix) together with a messege that this is a cached data.
+##  This function calls the different "makeCacheMatrix" functions according to the status of m. 
+##  When a new matrix is used m=null. Therefore the inverse matrix will be calculated and m will be set to the resut of this calculation (inverse matrix) and returned. 
+##  When the same matrix is called again m=invers matrix and therefore not null. ##  Therefore the function will simply return m (inverse matrix) together with a messege that this is a cached data.
 
 ## Returnes the inverse matrix of x
 
